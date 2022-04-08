0056b9727f20aa11f603a87c9c2964d044945715
fjenks17 <90281043+fjenks17@users.noreply.github.com>
2022-04-08 10:01:06 -0400

    Adding Docs Part 2: Electric Boogaloo

---
+++ API_Refs/html/class_automation_a_p_i_1_1_cube_builder.js
@@ -0,0 +1,8 @@
+var class_automation_a_p_i_1_1_cube_builder =
+[
+    [ "CubeBuilderTypes", "class_automation_a_p_i_1_1_cube_builder.html#a9e72c7f869caebb1c9ed21ce6fd1bd86", [
+      [ "TypesTwoPointsAndHeight", "class_automation_a_p_i_1_1_cube_builder.html#a9e72c7f869caebb1c9ed21ce6fd1bd86a20b6d6a5e6a502e3f657b753c7a4c5dd", null ],
+      [ "TypesDiagonalPoints", "class_automation_a_p_i_1_1_cube_builder.html#a9e72c7f869caebb1c9ed21ce6fd1bd86a64fc3a63094e0456ee8b7a6c76b59078", null ]
+    ] ],
+    [ "Commit", "class_automation_a_p_i_1_1_cube_builder.html#a3732d80222a656c1252a5db5cc2c5b9b", null ]
+];
---
+++ API_Refs/html/hierarchy.js
@@ -0,0 +1,21 @@
+var hierarchy =
+[
+    [ "AutomationAPI::FeatureCollection", "class_automation_a_p_i_1_1_feature_collection.html", null ],
+    [ "AutomationAPI::IBuilder", "class_automation_a_p_i_1_1_i_builder.html", [
+      [ "AutomationAPI::BlockBuilder", "class_automation_a_p_i_1_1_block_builder.html", null ],
+      [ "AutomationAPI::CubeBuilder", "class_automation_a_p_i_1_1_cube_builder.html", null ],
+      [ "AutomationAPI::WireBuilder", "class_automation_a_p_i_1_1_wire_builder.html", null ]
+    ] ],
+    [ "AutomationAPI::ICADObject", "class_automation_a_p_i_1_1_i_c_a_d_object.html", [
+      [ "AutomationAPI::CADObject", "class_automation_a_p_i_1_1_c_a_d_object.html", [
+        [ "AutomationAPI::Block", "class_automation_a_p_i_1_1_block.html", null ],
+        [ "AutomationAPI::Cube", "class_automation_a_p_i_1_1_cube.html", null ],
+        [ "AutomationAPI::Extrude", "class_automation_a_p_i_1_1_extrude.html", null ],
+        [ "AutomationAPI::Wire", "class_automation_a_p_i_1_1_wire.html", null ]
+      ] ]
+    ] ],
+    [ "AutomationAPI::Part", "class_automation_a_p_i_1_1_part.html", null ],
+    [ "AutomationAPI::PartCollection", "class_automation_a_p_i_1_1_part_collection.html", null ],
+    [ "AutomationAPI::RoutingCollection", "class_automation_a_p_i_1_1_routing_collection.html", null ],
+    [ "AutomationAPI::Session", "class_automation_a_p_i_1_1_session.html", null ]
+];
