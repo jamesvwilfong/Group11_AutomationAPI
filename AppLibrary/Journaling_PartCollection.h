bde679b91acfa411ad7623cd7ee13e20b11d41d2
Stone Soward <stone.soward@saggezza.com>
2022-04-07 23:22:46 -0400

    Journaling

---
+++ AppLibrary/Journaling_PartCollection.h
@@ -0,0 +1,12 @@
+#pragma once
+//#include "AppFeaturesOpsExports.h"
+
+namespace Application
+{
+	class Block;
+	class PartFile;
+	class BlockBuilder;
+}
+
+
+//APPLIBRARY_API Application::CubeBuilder* Journaling_FeatureCollection_CreateCubeBuilder(Application::PartFile *part, Application::Cube* cube);
--- AutomationBinding/PartCollection.cpp
+++ AutomationBinding/PartCollection.cpp
@@ -1,6 +1,7 @@
 #include "PartCollection.h"
 #include "Cube.h"
 #include "CubeBuilder.h"
+#include "..\AppLibrary\Journaling_PartCollection.h"
 #include <iostream>
 
 AutomationAPI::PartCollection::PartCollection(int guid) : m_guid(guid)
@@ -15,6 +16,10 @@ AutomationAPI::PartCollection::~PartCollection()
 
 AutomationAPI::CubeBuilder* AutomationAPI::PartCollection::CreateCubeBuilder(AutomationAPI::Cube* cube)
 {
+	
+	AutomationAPI::Cube* appCube = nullptr;
+	
+
 	if (cube == nullptr)
 	{
 		std::cout << "Cube is creation mode" << std::endl;
@@ -23,6 +28,11 @@ AutomationAPI::CubeBuilder* AutomationAPI::PartCollection::CreateCubeBuilder(Aut
 	{
 		std::cout << "Cube is edit/query mode" << std::endl;
 	}
+
+	
+	//AutomationAPI::CubeBuilder* cubeBuilder =
+	//	Journaling_PartCollection_CreateCubeBuilder(cube, appCube);
+	
 
 
 	return nullptr;
